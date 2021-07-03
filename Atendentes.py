f7312597c808cb86047f84e757948ba55e7ba79a
Leonardo Costa <79278612+leoncosta1980@users.noreply.github.com>
2021-07-03 08:38:16 -0300

    Delete Atendentes.py

--- Atendentes.py
+++
@@ -1,7 +0,0 @@
-class Atendentes:
-    def __init__(self, codigo, nome, data_nascimento, salario, turno):
-        self.nome = nome
-        self.codigo = codigo
-        self.data_nascimento = data_nascimento
-        self.__salario = salario
-        self.turno = turno
