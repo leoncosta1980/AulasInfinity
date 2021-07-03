1177d010be710b6ac386d641d9ea5e424cc6e8ca
Leonardo Costa <79278612+leoncosta1980@users.noreply.github.com>
2021-07-03 08:38:24 -0300

    Delete Clientes.py

--- Clientes.py
+++
@@ -1,6 +0,0 @@
-class Clientes:
-    def __init__(self, matricula, nome, data_nascimento, plano):
-        self.matricula = matricula
-        self.nome = nome
-        self.data_nascimento = data_nascimento
-        self.plano = plano
