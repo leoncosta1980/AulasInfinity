2c223c4ecbd5ec25499102e1a9ffae64f69d02f7
Leonardo Costa <79278612+leoncosta1980@users.noreply.github.com>
2021-07-23 20:45:21 -0300

    Projeto Serviço de Bordo

---
+++ Trabalho Infinity/Main1.py
@@ -0,0 +1,7 @@
+from Almoço import Almoço
+from Jantar import Jantar
+
+a1 = Almoço("Filé","batata","arroz", "","","")
+j1 = Jantar("Fraldinha","","","","","","")
+print(a1._proteinas)
+print(j1._proteinas)
