3daa41998b048f471eb7f6c254bc3e8a0648b1d7
Leonardo Costa <79278612+leoncosta1980@users.noreply.github.com>
2021-10-16 12:37:23 -0300

    Add files via upload

--- Aula 16_10/iris.py
+++ Aula 16_10/iris.py
@@ -38,9 +38,27 @@ for i in range(1,15):
 
 kmeans = KMeans(n_clusters = 3, init='k-means++', n_init = 10, max_iter = 300)
 clusters = kmeans.fit_predict(df)
-print(clusters)
+print("meus clusters:", clusters)
 df['target'] = clusters
-print(df.head(25))
+#print(df.head(25))
 
-sns.pairplot(df, hue='target')
-plt.show()
+#sns.pairplot(df, hue='target')
+
+
+#print("clusters originais:", clusters)
+#df['origin_target']=iris.target
+#sns.pairplot(df, hue='origin_target')
+#plt.show()
+
+df_cluster0= df[df['target']==0]
+df_cluster1= df[df['target']==1]
+df_cluster2= df[df['target']==2]
+
+print(len(df_cluster0))
+print(len(df_cluster1))
+print(len(df_cluster2))
+
+df_cluster0.to_csv('cluster1.csv', sep=',')
+df_cluster1.to_csv('cluster2.csv', sep=',')
+df_cluster2.to_csv('cluster3.csv', sep=',')
+                   
