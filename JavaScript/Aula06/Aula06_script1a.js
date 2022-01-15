4845fc7556070ad900cbb9aa5b04c59c24f99982
Leonardo Costa <79278612+leoncosta1980@users.noreply.github.com>
2022-01-15 12:39:48 -0300

    Add files via upload

---
+++ JavaScript/Aula06/Aula06_script1a.js
@@ -0,0 +1,7 @@
+var botao2 = document.getElementById('bt2');
+botao2.onclick = mudaParagrafo2;
+
+function mudaParagrafo2() {
+    document.getElementById('prgf2').innerHTML=
+        'Você clicou no botão!';
+}
---
+++ JavaScript/Aula06/Aula06_script1b.js
@@ -0,0 +1,7 @@
+var botao3 = document.getElementById('bt3');
+botao3.addEventListener("click", mudaParagrafo3);
+
+function mudaParagrafo3() {
+    document.getElementById('prgf3').innerHTML=
+        'Você clicou no botão!';
+}
